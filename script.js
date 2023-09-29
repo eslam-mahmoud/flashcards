e01d73b5b941b3dd727e124146194ef4fac3957b
Eslam Mahmoud <mail.eslammahmoud@gmail.com>
2023-09-29 11:18:17 -0500

    init

---
+++ script.js
@@ -0,0 +1,134 @@
+let currentAnswer;
+let lastQuestion = {
+    operation: null,
+    mainNumber: null,
+    randomNum: null
+};
+
+function checkAnswer() {
+    const userAnswer = parseFloat($("#answer").val());
+
+    if (userAnswer === currentAnswer) {
+        $("#feedback").text("Correct!").css("color", "green");
+    } else {
+        $("#feedback").text(`Wrong! The correct answer is ${currentAnswer}.`).css("color", "red");
+    }
+
+    $("#newQuestion").show();
+    $("#feedback").show();
+}
+
+function generateQuestion() {
+    $("#answer").val("");
+
+    const operations = $(".operation:checked").map(function() { return this.value; }).get();
+    const mainNumbers = $(".main-number:checked").map(function() { return parseInt(this.value); }).get();
+    const max = parseInt($("#maxNumber").val());
+
+    let selectedOperation;
+    let selectedMainNumber;
+    let randomNum;
+
+    do {
+        selectedOperation = operations[Math.floor(Math.random() * operations.length)];
+        selectedMainNumber = mainNumbers[Math.floor(Math.random() * mainNumbers.length)];
+
+        switch(selectedOperation) {
+            case "+":
+                randomNum = Math.floor(Math.random() * (max + 1));
+                break;
+            case "-":
+                randomNum = Math.floor(Math.random() * (max + 1));
+                break;
+            case "*":
+                randomNum = Math.floor(Math.random() * (max + 1));
+                break;
+            case "/":
+                do {
+                    randomNum = Math.floor(Math.random() * (max + 1));
+                } while(randomNum === 0); // Ensure randomNum is not zero
+                break;
+        }
+    } while (
+        selectedOperation === lastQuestion.operation &&
+        selectedMainNumber === lastQuestion.mainNumber &&
+        randomNum === lastQuestion.randomNum
+    );
+
+    lastQuestion = {
+        operation: selectedOperation,
+        mainNumber: selectedMainNumber,
+        randomNum: randomNum
+    };
+
+    switch(selectedOperation) {
+        case "+":
+            $("#question").text(`${selectedMainNumber} + ${randomNum} = ?`);
+            currentAnswer = selectedMainNumber + randomNum;
+            break;
+        case "-":
+            $("#question").text(`${selectedMainNumber} - ${randomNum} = ?`);
+            currentAnswer = selectedMainNumber - randomNum;
+            break;
+        case "*":
+            $("#question").text(`${selectedMainNumber} x ${randomNum} = ?`);
+            currentAnswer = selectedMainNumber * randomNum;
+            break;
+        case "/":
+            $("#question").text(`${selectedMainNumber} ÷ ${randomNum} = ?`);
+            currentAnswer = selectedMainNumber / randomNum;
+            break;
+    }
+
+    // Display game elements and hide settings
+    $("#question, #answer, #submitAnswer").show();
+    $("#newQuestion").hide();
+    $("#feedback").text("");
+    $("#answer").focus();
+}
+
+function populateMaxNumbers() {
+    for (let i = 1; i <= 100; i++) {
+        $('#maxNumber').append(`<option value="${i}">${i}</option>`);
+    }
+}
+
+$(document).ready(function() {
+    populateMaxNumbers();
+
+    $("#submitAnswer").on("click", function() {
+        checkAnswer();
+    });
+
+    $("#newQuestion").on("click", function() {
+        generateQuestion();
+    });
+
+    $("#answer").on("keypress", function(e) {
+        if (e.which === 13) {
+            e.preventDefault();
+            checkAnswer();
+        }
+    });
+
+    $("#startButton").on("click", function(e) {
+        e.preventDefault();
+        $("#settings").hide();
+        $("#game").show();
+        generateQuestion();
+    });
+
+    $("#backLink").on("click", function(e) {
+        e.preventDefault();
+        $("#settings").show();
+        $("#game").hide();
+        $("#question, #answer, #submitAnswer, #feedback, #newQuestion").hide();
+
+        // Reset last question to ensure variety
+        lastQuestion = {
+            operation: null,
+            mainNumber: null,
+            randomNum: null
+        };
+    });
+});
