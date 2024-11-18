4f8a0dde5c797be86421c5fc435aa89404280135
Eslam Mahmoud <mail.eslammahmoud@gmail.com>
2024-11-18 12:10:46 +0200

    using claudia to create lambda file

--- index.mjs
+++
@@ -1,7 +0,0 @@
-import awsServerlessExpress from 'aws-serverless-express';
-import app from './app.mjs';
-const server = awsServerlessExpress.createServer(app)
-
-export const handler = (event, context) => {
-    awsServerlessExpress.proxy(server, event, context);
-};
